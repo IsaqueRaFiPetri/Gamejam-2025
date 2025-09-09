c098c1cc4d05248034c5935d426acc5ae8fbbd34
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-09-09 09:31:57 -0300

    Inimigo_Script_Collison

--- Gamejam25_Equipe8/Assets/Scripts/Enemy/Enemy.cs
+++ Gamejam25_Equipe8/Assets/Scripts/Enemy/Enemy.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform playerTransform;  
-    public float moveSpeed = 3f;       
+    public Transform playerTransform;
+    public float moveSpeed = 3f;
+    public BoxCollider2D player_Collider;
 
     void Update()
     {
         if (playerTransform != null)
-        {           
+        {
             Vector2 direction = (playerTransform.position - transform.position).normalized;
-           
+
             transform.position += (Vector3)(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D player_Collider)
+    {
+        if (player_Collider.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Inimigo colidiu com o player!");
         }
     }
 }
