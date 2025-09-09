79bd53814e435f6035a135a1d54a2de0f53ab0d7
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-09-09 10:56:56 -0300

    Script_Enemy_Attack

--- Gamejam25_Equipe8/Assets/Scripts/Enemy/Enemy.cs
+++ Gamejam25_Equipe8/Assets/Scripts/Enemy/Enemy.cs
@@ -1,12 +1,19 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    public CharacterStatus enemyStatus;
     public Transform playerTransform;
     public float moveSpeed = 3f;
     public BoxCollider2D player_Collider;
+    private bool canDetectCollision = true;
+    public CharacterStatus EnemyStatus;
 
+    void Start()
+    {
+        EnemyStatus.life = EnemyStatus.maxLife;        
+    }
     void Update()
     {
         if (playerTransform != null)
@@ -17,11 +24,24 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter2D(Collision2D player_Collider)
+    void OnCollisionStay2D(Collision2D player_Collider)
     {
-        if (player_Collider.gameObject.CompareTag("Player"))
+        if(player_Collider.gameObject.CompareTag("Player") && canDetectCollision)
         {
             Debug.Log("Inimigo colidiu com o player!");
+            Attack();
+            canDetectCollision = false;
+            StartCoroutine(ResetCollisionDetection());
         }
+    }
+    IEnumerator ResetCollisionDetection()
+    {
+        yield return new WaitForSeconds(1f);
+        canDetectCollision = true;
+    }
+
+    void Attack()
+    {
+        PlayerStatus.instance.TakeDamage(enemyStatus.damage);
     }
 }
