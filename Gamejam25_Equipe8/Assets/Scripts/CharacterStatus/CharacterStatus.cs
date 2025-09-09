ff1a4a9680c9d84bddc44ef43e0c0c47fc44ca4b
isqu3 <irfpetri@gmail.com>
2025-09-09 10:14:25 -0300

    AddPlayerStatus

--- Gamejam25_Equipe8/Assets/Scripts/CharacterStatus/CharacterStatus.cs
+++ Gamejam25_Equipe8/Assets/Scripts/CharacterStatus/CharacterStatus.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CharacterStatus", menuName = "Scriptable Objects/CharacterStatus")]
 public class CharacterStatus : ScriptableObject
 {
+    [Header("Life")]
     public float life;
     public float maxLife;
     public float lifeRegen;
+    public float lifeRegenCooldown;
+
+    [Header("Energy")]
     public float energy;
     public float maxEnergy;
     public float energyRegen;
+    public float energyRegenCooldown;
+
+    [Header("others Variables")]
     public float moveSpeed;
     public float thougness;
     public float damage;
--- Gamejam25_Equipe8/Assets/Scripts/Player/PlayerStatus.cs
+++ Gamejam25_Equipe8/Assets/Scripts/Player/PlayerStatus.cs
@@ -1,13 +1,62 @@
 using UnityEngine;
-using Yarn;
+using UnityEngine.UI;
 
 public class PlayerStatus : MonoBehaviour
 {
+    public static PlayerStatus instance;
     [SerializeField] CharacterStatus playerStatus;
+    [SerializeField] Image lifeBar, energyBar;
+
+    float lifeRegenTimer;
+    float energyRegenTimer;
 
     private void Start()
     {
+        instance = this;
         playerStatus.life = playerStatus.maxLife;
         playerStatus.energy = playerStatus.maxEnergy;
+    }
+
+    private void Update()
+    {
+        lifeBar.fillAmount = playerStatus.life / playerStatus.maxLife;
+        energyBar.fillAmount = playerStatus.energy / playerStatus.maxEnergy;
+
+        if (Troca_Personagens.instance.isHappy)
+        {
+            RegenerateLife();
+            RegenerateEnergy();
+        }
+    }
+
+    void RegenerateLife()
+    {
+        lifeRegenTimer += Time.deltaTime;
+        if (lifeRegenTimer >= playerStatus.lifeRegenCooldown)
+        {
+            playerStatus.life = Mathf.Min(playerStatus.life + playerStatus.lifeRegen, playerStatus.maxLife);
+            lifeRegenTimer = 0f;
+            Debug.Log("Regenerou");
+        }
+    }
+
+    void RegenerateEnergy()
+    {
+        energyRegenTimer += Time.deltaTime;
+        if (energyRegenTimer >= playerStatus.energyRegenCooldown)
+        {
+            playerStatus.energy = Mathf.Min(playerStatus.energy + playerStatus.energyRegen, playerStatus.maxEnergy);
+            energyRegenTimer = 0f;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        playerStatus.life -= damage;
+    }
+
+    public void ReduceEnergy(int reduction)
+    {
+        playerStatus.energy -= reduction;
     }
 }
