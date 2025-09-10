7f0553892995eee56b64835cb08993e31d066c5b
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-09-10 11:23:46 -0300

    Audio_Movimentacao

--- Gamejam25_Equipe8/Assets/Scripts/Player/PlayerMovTopdown.cs
+++ Gamejam25_Equipe8/Assets/Scripts/Player/PlayerMovTopdown.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerMovTopdown : MonoBehaviour
 {
@@ -7,14 +8,61 @@ public class PlayerMovTopdown : MonoBehaviour
     Rigidbody2D rb;
     Vector2 movementInput;
 
-    private void Start()
+    public AudioClip[] sonsDePassos;
+    AudioSource audioSource;
+
+    Coroutine passosCoroutine;
+    bool isMoving = false;
+    int passoIndex = 0;
+
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.playOnAwake = false;
+        audioSource.loop = false;
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
+        bool currentlyMoving = movementInput != Vector2.zero;
+
+        if (currentlyMoving && !isMoving)
+        {
+            isMoving = true;
+            passosCoroutine = StartCoroutine(TocarPassosEmOrdem());
+        }
+        else if (!currentlyMoving && isMoving)
+        {
+            isMoving = false;
+            if (passosCoroutine != null)
+                StopCoroutine(passosCoroutine);
+        }
+    }
+
+    IEnumerator TocarPassosEmOrdem()
+    {
+        passoIndex = 0;
+
+        while (true)
+        {
+            float delay = Troca_Personagens.instance.isFear ? 0.25f : 0.465f;
+
+            if (sonsDePassos.Length > 0)
+            {
+                audioSource.PlayOneShot(sonsDePassos[passoIndex]);
+
+                passoIndex++;
+                if (passoIndex >= sonsDePassos.Length)
+                    passoIndex = 0;
+            }
+
+            yield return new WaitForSeconds(delay);
+        }
     }
 
     private void FixedUpdate()
@@ -26,7 +74,6 @@ public class PlayerMovTopdown : MonoBehaviour
         else
             rb.linearVelocity = movementInput * playerStatus.moveSpeed;
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Enemy>())
@@ -35,3 +82,6 @@ public class PlayerMovTopdown : MonoBehaviour
         }
     }
 }
+
+
+
