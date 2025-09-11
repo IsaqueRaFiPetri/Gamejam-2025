2ecf2cbbc5034ca627d870318f21121e679f7524
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-09-11 10:59:38 -0300

    Boss_ambiente_sound

--- Gamejam25_Equipe8/Assets/Scripts/Som_boss_ambiente.cs
+++ Gamejam25_Equipe8/Assets/Scripts/Som_boss_ambiente.cs
@@ -4,24 +4,40 @@ public class Som_boss_ambiente : MonoBehaviour
 {
     public AudioClip battle1;
     public AudioClip battle2;
+    public AudioClip intro;
+    public AudioClip theend;
 
     private AudioSource audiobattle1;
     private AudioSource audiobattle2;
-    
+    private AudioSource audioIntro;
+    private AudioSource audioTheend;
+
     void Start()
     {
         audiobattle1 = gameObject.AddComponent<AudioSource>();
         audiobattle2 = gameObject.AddComponent<AudioSource>();
+        audioIntro = gameObject.AddComponent<AudioSource>();
+        audioTheend = gameObject.AddComponent<AudioSource>();
 
         audiobattle1.clip = battle1;
         audiobattle1.loop = true;
-        audiobattle1.volume = 0.05f;
+        audiobattle1.volume = 0.001f;
         audiobattle1.Play();
 
         audiobattle2.clip = battle2;
         audiobattle2.loop = true;
-        audiobattle2.volume = 0.5f;
+        audiobattle2.volume = 0.25f;
         audiobattle2.Play();
+
+        audioIntro.clip = intro;
+        audioIntro.loop = true;
+        audioIntro.volume = 0.025f;
+        audioIntro.Play();
+
+        audioTheend.clip = theend;
+        audioTheend.loop = true;
+        audioTheend.volume = 0.8f;
+        audioTheend.Play();
     }
 
 }
