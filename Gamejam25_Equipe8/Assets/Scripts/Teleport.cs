573a79249d350ff58482291008fdbda3df9ba97a
Bernardorocha0710 <167346087+Bernardorocha0710@users.noreply.github.com>
2025-09-11 11:01:32 -0300

    Teleport_area

--- Gamejam25_Equipe8/Assets/Scripts/Teleport.cs
+++ Gamejam25_Equipe8/Assets/Scripts/Teleport.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Teleport : MonoBehaviour
 {
-    [SerializeField] private Transform destino; 
+    [SerializeField] private Transform destino;
+    [SerializeField] private UnityEvent onTeleport;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.position = destino.position; 
+            other.transform.position = destino.position;
+            onTeleport?.Invoke();
         }
     }
 }
